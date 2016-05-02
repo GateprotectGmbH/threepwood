472e5bafb92af0645237bee1ff6d438fd54f3b71
Mike Park <michael.park.ext@gateprotect.com>
2016-05-02 14:01:31 +0200

    screen paging and font size scaling
    Refs #68000

--- src/app/branch-card.ts
+++ src/app/branch-card.ts
@@ -1,7 +1,27 @@
 import 'angular';
+import {Branch} from "./builds";
+
+class BranchCtrl {
+  branch:Branch;
+
+  // scale long project name lists to smaller font size
+  projectNameFontSize():string {
+    let MIN_SCALE = 0.6;
+    let MAX_SCALE = 1.6;
+    let NORMAL_LENGTH = 40;
+    let maxLength = this.branch.projectNames().map(name => name.length).reduce((a, b) => a+b);
+    let scale = MAX_SCALE;
+    if (maxLength > NORMAL_LENGTH) {
+      scale = scale * NORMAL_LENGTH / maxLength;
+    }
+    scale = Math.max(MIN_SCALE, scale);
+    return `${scale}vh`;
+  }
+}
 
 const BranchCardComponent:ng.IComponentOptions = {
   templateUrl: 'src/app/branch-card.html',
+  controller: BranchCtrl,
   bindings: {
     branch: '<'
   }
--- src/app/dashboard.ts
+++ src/app/dashboard.ts
@@ -12,14 +12,17 @@ class Dashboard {
   static PROJECTS_RELOAD_INTERVAL = 60 * 60 * 1000; // ms
   static PROJECTS_ERROR_RETRY = 60 * 1000; // ms
   static BUILDS_RELOAD_INTERVAL = 27 * 1000; // ms
+  static NEXT_PAGE_INTERVAL = 11 * 1000; // ms
   static $inject = ['buildsService', '$q', '$scope', '$interval'];
 
   projects:Project[] = [];
   branches:Branch[] = [];
+  branchOffset:number = 0;
+  rows = [0, 1, 2];
 
   loading:string = 'Loading matching projects..';
   skipLoadBranchSummary:boolean;
-  
+
   config:SettingsConfig;
 
   constructor(private buildsService:BuildsService,
@@ -32,12 +35,32 @@ class Dashboard {
     // auto reload
     $interval(this.loadProjects.bind(this), Dashboard.PROJECTS_RELOAD_INTERVAL);
     $interval(this.loadBranchSummaries.bind(this), Dashboard.BUILDS_RELOAD_INTERVAL);
+    $interval(this.nextPage.bind(this), Dashboard.NEXT_PAGE_INTERVAL);
 
     // settings have changed, reload..
     $scope.$on('reload:projects', () => {
       this.loading = 'Reloading projects..';
       this.loadProjects();
     });
+  }
+
+  nextPage() {
+    this.branchOffset += 9;
+  }
+
+  row(rowIndex:number):Branch[] {
+    if (this.branchOffset >= this.branches.length) {
+      this.branchOffset = 0;
+    }
+    let branches = [];
+    let startIndex = this.branchOffset + (rowIndex * 3);
+    let lastIndex = startIndex + 3;
+    for (let i = startIndex; i < lastIndex; i++) {
+      if (this.branches[i]) {
+        branches.push(this.branches[i]);
+      }
+    }
+    return branches;
   }
 
   loadProjects() {
@@ -57,7 +80,7 @@ class Dashboard {
         console.log('project load failed.. retrying..');
         // if already showing a message, then continue, otherwise stay silent
         if (this.loading) {
-          this.loading = `Failed to load projects. Retrying in ${Dashboard.PROJECTS_ERROR_RETRY/1000} seconds..`;
+          this.loading = `Failed to load projects. Retrying in ${Dashboard.PROJECTS_ERROR_RETRY / 1000} seconds..`;
         }
         this.$interval(this.loadProjects.bind(this), Dashboard.PROJECTS_ERROR_RETRY, 1);
       });
