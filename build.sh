da7f28c3cc311731d16f5fa635e5de880f3b653e
Mike Park <michael.park.ext@gateprotect.com>
2016-05-02 14:16:08 +0200

    build & gitlab cleanup
    Refs #68000

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export JSPM_GITHUB_AUTH_TOKEN=Z3BtaWtlcDpkNDYyZjc5OWU0ODAwYmI3MjAyZWRjM2ZhZWI2Yzg2MDZiNzg5MmM0
-npm install && npm run deps && npm run build && npm run package
--- gitlab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# executed in gitlab-ci
-# default location of code is project dependent (example /builds/Command.Center/eye)
-# copy code to standard place that uses caching
-cp -R . /src
-cd /src && ./ci.sh
